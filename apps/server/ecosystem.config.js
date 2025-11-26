b8fc78cb457a1e519bc5449e8cb39c8b36ca1a23
dohard@163.com <dohard@163.com>
2025-11-26 08:02:40 +0800

    pm2 config

---
+++ apps/server/ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [
+        {
+            name: 'huajianli',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start --keepAliveTimeout 5000', // 添加 keepAliveTimeout 参数
+            instances: 1,
+            exec_mode: 'cluster',
+            watch: false,
+            env: {
+                PORT: 8082,
+                NODE_ENV: 'production',
+                TZ: 'Asia/Shanghai',
+            },
+            env_production: {
+                PORT: 8082,
+                NODE_ENV: 'production',
+            },
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            error_file: 'logs/pm2/error.log',
+            out_file: 'logs/pm2/out.log',
+            merge_logs: true,
+            max_memory_restart: '1G',
+        },
+    ],
+}
